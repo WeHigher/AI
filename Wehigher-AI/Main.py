7b1dee7a0fad44fb36084944a6582ac1d0b697c6
kwan2 <74182484+kwan2@users.noreply.github.com>
2023-09-07 20:26:55 +0900

    feat: sentence API

--- Wehigher-AI/Main.py
+++ Wehigher-AI/Main.py
@@ -2,12 +2,18 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 import numpy as np
 from tensorflow.keras.models import load_model
+from dotenv import load_dotenv
+import os
+import openai
 
 app = Flask(__name__)
 CORS(app)
+load_dotenv('.env.local')
+API_KEY = os.getenv('API_KEY')
+openai.api_key = API_KEY
 
 @app.route('/sign', methods=['POST'])
-def get_items():
+def wordRecognition():
 
     data = request.get_json()  # JSON 데이터를 받아옴
 
@@ -38,6 +44,34 @@ def get_items():
 
     return predictions
 
+
+@app.route('/sentense', methods=['POST'])
+def sentenceCreate():
+    data = request.get_json()
+
+    # 모델 - GPT 3.5 Turbo 선택
+    model = "gpt-3.5-turbo"
+
+    # 질문 작성하기
+    query = '해당 단어를 토대로 문장을 만들어주세요.'
+    for word in data:
+        query += f"{word}, "
+
+    query = query[:-2]
+
+    # 메시지 설정하기
+    messages = [
+        {"role": "system", "content": "경어체로 답변하고, 한글로만 답변해주세요."},
+        {"role": "user", "content": query}
+    ]
+
+    # ChatGPT API 호출하기
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages
+    )
+
+    return jsonify(response)
 if __name__ == '__main__':
     app.run(debug=True)
 
